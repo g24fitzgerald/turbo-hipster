5e5d0e0f93f8ecc621fb16e25f1171a616e18a0b
Gina Fitzgerald <Gina@Ginas-MacBook-Air.local>
2014-08-23 14:46:08 -0700

    medical warning
    warns user against performing operation without training

---
+++ project_workout_warningbox.py
@@ -0,0 +1,8 @@
+import tkinter.messagebox 
+
+
+tkinter.messagebox.showwarning("show warning", "this workout is not intended for those without previous training. Under no circumstances should these exercises be performed without first having recieved proper training from an instructor. This is not a tutorial on becoming a contortionist, rather this is a workout generator for contortionists seeking to vary their personal training. By clicking 'ok' you assume responsibility for any and all injuries aquired while following this program.")
+                       
+
+                                   
+
