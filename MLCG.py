fe01b5f0904975dffb41988c2fc6691f82410619
Gina Fitzgerald <Gina@Ginas-MacBook-Air.local>
2014-09-02 08:21:46 -0700

    MLCG
    pseudorandom number generator

---
+++ MLCG.py
@@ -0,0 +1,26 @@
+#Copyright (c) 2014 Gina Fitzgerald
+#Multiplicative Linear Congruential Generator: pesudorandom number generator
+
+#seed
+s =17
+
+def randrange(j1, j2 = None):
+    global s
+    a = 47
+    m = 277
+
+    if j2 == None:
+        low = 0
+        high = j1
+    else:
+        low = j1
+        high = j2
+#result
+r = s % (high - low) + low
+
+#update seed value
+
+s = (s * a) % m
+
+return r
+
